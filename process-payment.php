654e7d9c00967d23fb98d1bef39fc233f328096b
vihugarcia <vihugarcia@gmail.com>
2020-08-21 01:24:54 -0300

    third draft

--- process-payment.php
+++ process-payment.php
@@ -14,7 +14,6 @@ else {
 
 $image = $protocol . $_SERVER['SERVER_NAME'] . $_POST['img'];
 
-MercadoPago\SDK::setPublicKey('APP_USR-7eb0138a-189f-4bec-87d1-c0504ead5626');
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
